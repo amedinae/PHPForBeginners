57ef83b1bef2181194aa065fe68cd5aeffd507a1
amedinae <31489998+amedinae@users.noreply.github.com>
2023-03-20 01:08:37 +0100

    Initial commit at ep15

---
+++ views/404.php
@@ -0,0 +1,9 @@
+<?php require "partials/head.php" ?>
+    <?php require "partials/nav.php"?>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <h1 class="text-2xl font-bold">Sorry: Page not found</h1>
+        <p class="mt-4">You can go back to the <a href="/" class="text-blue underline">home page</a>.</p>
+    </div>
+</main>
+<?php require "partials/footer.php" ?>
